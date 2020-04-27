869f6a489573bb47fac8951ae4a78063eeb65328
Dois2 <43417318+Dois2@users.noreply.github.com>
2020-04-27 20:32:32 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+
+// import SignUp from './pages/SignUp';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
+// Todo componente dentro do Provider vai ter acesso as informações do contexto
+
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </Router>
+);
+
+export default App;
