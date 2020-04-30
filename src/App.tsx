82e973827a4e0873acaeac809cd8d391ac9a9b85
root <root@DESKTOP-AJCRDJA.localdomain>
2020-04-30 14:19:34 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
-// import SignUp from './pages/SignUp';
-
 import AppProvider from './hooks';
 
 import Routes from './routes';
